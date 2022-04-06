ed8f59eebe3e8981880f268b9b69678f416423cc
Yaroslav63 <era6300@mail.ru>
2022-04-06 18:07:19 +0300

    add Task 47

---
+++ Task47/Program.cs
@@ -0,0 +1,11 @@
+﻿double[,] a = new double[3, 4];
+Random random = new Random();
+for (int i = 0; i < 3; i++)
+{
+    for (int j = 0; j < 4; j++)
+    {
+        a[i, j] = random.Next(-10, 10) + random.NextDouble();
+        Console.Write("{0,5:F1}\t", a[i, j]);
+    }
+    Console.WriteLine();
+}
